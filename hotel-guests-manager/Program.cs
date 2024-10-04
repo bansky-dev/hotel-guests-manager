dd951b4145defd9240f46a8ed642ef6de3352eff
Kociarz <dbednorz@tuta.io>
2024-10-04 20:09:50 +0200

    first commit

---
+++ hotel-guests-manager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace hotel_guests_manager;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+    }
+
+    //TODO: finish in the morning....
+
+    static void connectToDatabase(){
+        string constr;
+
+        SqlConnection connection;
+
+        constr = @"Data source=localhost;Initial Catalog=hotel;User ID= root@localhost;Password=24518300";
+
+        connection = new SqlConnection(constr);
+
+    }
+}
